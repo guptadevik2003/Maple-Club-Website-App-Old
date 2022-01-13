fe33a0f324cf6d13b0a1de2cc639ebbbf238f2aa
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2022-01-14 01:25:36 +0530

    Update appConfig.js

--- functions/appConfig.js
+++ functions/appConfig.js
@@ -13,6 +13,7 @@ module.exports = ({ app, express }) => {
 
         app.use(express.static(`${process.cwd()}/views`))
         app.set('view-engine', 'ejs')
+        app.use(express.json())
         app.use(express.urlencoded({ extended: false }))
         app.use(
             session({
